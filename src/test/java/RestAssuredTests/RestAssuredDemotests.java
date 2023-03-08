006f6d61c80938a848a717cc2c1276548b944e17
masilingam123 <k.masilingam@gmail.com>
2023-03-08 16:39:02 +0530

    new test case

--- src/test/java/RestAssuredTests/RestAssuredDemotests.java
+++ src/test/java/RestAssuredTests/RestAssuredDemotests.java
@@ -24,8 +24,7 @@ public class RestAssuredDemotests {
 	@Test
 	public void getSingleUser()
 	{		
-		RestAssured.basePath="/api/users/2";
-		
+		RestAssured.basePath="/api/users/2";		
 		Response response=
 		given()
 		.when()
@@ -36,15 +35,12 @@ public class RestAssuredDemotests {
 		   .assertThat().body("data.id", equalTo(2))
 		   .header("content-type", "application/json; charset=utf-8").extract().response();
 		Assert.assertTrue(util.jsonStringComparison(response, "janet.weaver@reqres.in"));
-//		String jsonstring=response.asString();
-//		Assert.assertEquals(jsonstring.contains("janet.weaver@reqres.in"), true);
 	}
 	
 	@Test
 	public void getListUsers()
 	{
 		RestAssured.basePath="/api/users?page=1";
-
 		given()
 		.when()
 		   .get()
@@ -52,10 +48,7 @@ public class RestAssuredDemotests {
 		   .statusCode(util.statuscode)
 		   .statusLine(util.statusline)
 		   .body("data.id", hasItems(1,2,3,4,5,6))
-		   .header("content-type", "application/json; charset=utf-8").log().all();
-		
-
-		
+		   .header("content-type", "application/json; charset=utf-8").log().all();		
 	}
 	
 	@Test
@@ -63,24 +56,19 @@ public class RestAssuredDemotests {
 	{
 		util=new Util(false);
 		RestAssured.basePath="/api/users/23";
-
 		given()
 		.when()
 		   .get()
 		.then()
 		   .statusCode(util.statuscode)
 		   .statusLine(util.statusline)
-		   .header("content-type", "application/json; charset=utf-8");
-		
-
-		
+		   .header("content-type", "application/json; charset=utf-8");		
 	}
 
 	@Test
 	public void getDelayedResponse()
 	{
 		RestAssured.basePath="/api/users?delay=3";
-
 		given()
 		.when()
 		   .get()
@@ -88,17 +76,13 @@ public class RestAssuredDemotests {
 		   .statusCode(util.statuscode)
 		   .statusLine(util.statusline)
 		   .body("data.id", hasItems(1,2,3,4,5,6))
-		   .header("content-type", "application/json; charset=utf-8");
-		
-
-		
+		   .header("content-type", "application/json; charset=utf-8");		
 	}
 	
 	@Test
 	public void getSingleResource()
 	{
 		RestAssured.basePath="/api/unknown/2";
-
 		given()
 		.when()
 		   .get()
@@ -106,29 +90,18 @@ public class RestAssuredDemotests {
 		   .statusCode(util.statuscode)
 		   .statusLine(util.statusline)
 		   .body("data.color",equalTo("#C74375"))
-		   .header("content-type", "application/json; charset=utf-8");
-		
-
+		   .header("content-type", "application/json; charset=utf-8");	
 		
 	}
-	
-//	@Test
-//	public void sample()
-//	{
-//		given().when().get("https://dummy.restapiexample.com/api/v1/employees").then().statusCode(200).log().all();
-//	}
-//	
-//	@Test
-//	public void post()
-//	{		
-//		HashMap map=new HashMap();
-//		map.put("name", "morpheus");
-//		map.put("job", "leader");
-////		map.forEach((k, v) -> {
-////			System.out.println("Key: " + k + ", Value: " + v);
-////		});
-//		given().contentType("application/json; charset=utf-8").body(map).post("/api/users").then().statusCode(201);
-//	}
-
-
+	@Test
+	public void getSingleResource7()
+	{
+		RestAssured.basePath="/api/unknown/7";
+		given()
+		.when()
+		   .get()
+		.then()
+		   .statusCode(util.statuscode)
+		   .header("content-type", "application/json; charset=utf-8");		
+	}
 }
